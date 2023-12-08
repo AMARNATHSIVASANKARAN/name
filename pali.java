c2694bb5bcad4a38642fae727202fbe2293a3f70
amarnath <amar_sivasankaran@thbs.com>
2023-12-08 14:57:31 +0530

    done

---
+++ pali.java
@@ -0,0 +1,28 @@
+public class Pali {
+
+    public static void main(String[] args) {
+        String input = "level";
+        
+        if (isPalindrome(input)) {
+            System.out.println(input + " is a palindrome.");
+        } else {
+            System.out.println(input + " is not a palindrome.");
+        }
+    }
+
+    static boolean isPalindrome(String str) {
+        // Remove spaces and convert to lowercase for case-insensitive palindrome check
+        str = str.replaceAll("\\s", "").toLowerCase();
+        
+        int length = str.length();
+        
+        for (int i = 0; i < length / 2; i++) {
+            if (str.charAt(i) != str.charAt(length - i - 1)) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
+
